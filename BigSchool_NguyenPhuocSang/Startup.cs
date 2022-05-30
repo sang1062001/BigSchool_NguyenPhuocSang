00bedd78094c94bd0acc7d58324088e808edf711
PC <PC@LAPTOP-TBEI58HB>
2022-05-30 16:11:40 +0700

    Add project files.

---
+++ BigSchool_NguyenPhuocSang/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool_NguyenPhuocSang.Startup))]
+namespace BigSchool_NguyenPhuocSang
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ BigSchool_NguyenPhuocSang/ViewModels/CourseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BigSchool_NguyenPhuocSang.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool_NguyenPhuocSang.ViewModels
+{
+    public class CourseViewModel
+    {
+        [Required]
+        public string Place { get; set; }
+        public string Date{ get; set; }
+        public string Time { get; set; }
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
