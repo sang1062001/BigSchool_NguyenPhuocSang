cffd20f83909898514ac3a3caef38e83d2f655fb
PC <PC@LAPTOP-TBEI58HB>
2022-06-13 16:12:43 +0700

    bosung bigschool63

---
+++ BigSchool_NguyenPhuocSang/DTOs/AttendanceDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool_NguyenPhuocSang.DTOs
+{
+    public class AttendanceDto
+    {
+        public int CourseId { get; set; }
+    }
+}
